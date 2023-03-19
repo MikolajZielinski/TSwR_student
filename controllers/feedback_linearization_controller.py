1583dc4e2211526a2acf293d744c9063f132816b
MikolajZielinski <mikolaj.m.zielinski@student.put.poznan.pl>
2023-03-19 15:08:36 +0100

    Lab 1 finished

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -13,16 +13,15 @@ class FeedbackLinearizationController(Controller):
         robot state x and desired control v.
         """
         q1, q2, q1_dot, q2_dot = x
-        v = np.array(q_r_ddot + (0.0 * ([q1_dot, q2_dot] - q_r_dot)) + (0.0 * ([q1, q2] - q_r))).T
+        v = np.array(q_r_ddot + (1.0 * (q_r_dot - [q1_dot, q2_dot])) + (10.0 * (q_r - [q1, q2])))
 
         tau = self.model.M(x) @ v + self.model.C(x) @ np.array([q1_dot, q2_dot]).T
 
-        # print("------------------------------------------------------------------------------------------------------")
-        # print(x)
-        # print(q_r, q_r_dot, q_r_ddot)
-        # print([q1_dot, q2_dot], q_r_dot, [q1_dot, q2_dot] - q_r_dot)
-        # print(self.model.M(x), q_r_ddot, self.model.M(x) @ q_r_ddot)
-        # print(self.model.C(x), np.array([q1_dot, q2_dot]).T, self.model.C(x) @ np.array([q1_dot, q2_dot]).T)
-        # print("------------------------------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------------------------------")
+        print(q_r_ddot)
+        print([q1_dot, q2_dot], q_r_dot, ([q1_dot, q2_dot] - q_r_dot))
+        print([q1, q2], q_r, ([q1, q2] - q_r))
+        print((5.0 * ([q1, q2] - q_r)))
+        print("------------------------------------------------------------------------------------------------------")
         
         return tau
